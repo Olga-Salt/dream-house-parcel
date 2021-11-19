cf0f8938d42abea5cdb468930255cd5dfc5be289
Olga Saltanovskaya <saltanoffskaya@gmail.com>
2021-11-19 15:20:28 +0200

    add styles to search form, btn load more

--- src/js/create-photos-murkup.js
+++ src/js/create-photos-murkup.js
@@ -2,20 +2,20 @@ export default function createPhotosMurkup(photo) {
   return photo
     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
       return `
-  <a  href="${largeImageURL}"><div class="photo-card">
-  <img src="${webformatURL}" width=100 alt="${tags}" loading="lazy" />
+  <a class="gallery__item" href="${largeImageURL}"><div class="gallery__item-wrapper">
+  <img src="${webformatURL}" width=100 alt="${tags}" loading="lazy"  class="gallery__image" />
   <div class="info">
     <p class="info-item">
-      <b>${likes}</b>
+      <b>Likes</b><span class="info-value">${likes}</span>
     </p>
     <p class="info-item">
-      <b>${views}</b>
+      <b>Views</b><span class="info-value">${views}</span>
     </p>
     <p class="info-item">
-      <b>${comments}</b>
+       <b>Comments</b><span class="info-value">${comments}</span>
     </p>
     <p class="info-item">
-      <b>${downloads}</b>
+      <b>Downloads</b><span class="info-value">${downloads}</span>
     </p>
   </div>
 </div></a>
